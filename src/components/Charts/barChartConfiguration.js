28c8be1071ea0c68340d38006cacbd8b4ab99872
kevin <kevin.cvetezar@gmail.com>
2021-02-03 16:56:00 +0100

    Fix graph width problem.

--- src/components/Charts/barChartConfiguration.js
+++ src/components/Charts/barChartConfiguration.js
@@ -22,16 +22,18 @@ export const configue = (labels, label, data, backgroundColor, borderColor, id, 
       lineTension: 1,
       scales: {
         yAxes: [{
-          position: 'right',
+          afterFit: function(scaleInstance) {
+            scaleInstance.width = 210; // sets the width to 100px
+          },
           ticks: {
             beginAtZero: true,
             padding: 25,
             autoSkip: false,
             fontSize: 16,
-            /*callback: function (tick) {
-              if (tick.length > 12) return tick.substring(0, 9)+"...";
+            callback: function (tick) {
+              if (tick.includes("www.")) return tick.replace("www.", "");
               else return tick
-            }*/
+              }
           }
         }],
         xAxes: [{
